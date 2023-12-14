8866200a397d0ece9522aa2ca538fe17090ffe8a
daryna-budz <darynabudz@gmail.com>
2023-12-14 13:46:54 -0500

    math operations

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
-const radius = +prompt("Please enter radius");
-function calcSquare(radius){
-    return Math.pow(radius,2)*Math.PI;
+const num1 = +prompt("Enter first number");
+const num2 = +prompt("Enter second number");
+const operation = prompt("Enter math operation");
+
+function math(num1,num2,operator){
+    if(operator === "+"){
+        return num1+num2;
+    }else if(operator === "-"){
+        return num1-num2;
+    }else if (operator === "*"){
+        return num1*num2;
+    }else if (operator === "/"){
+        return num1/num2;
+    }else{
+        return 0;
+    }
 }
-console.log(calcSquare(radius))
+
+console.log(math(num1,num2,operation))
